3503cc41bfcb9038668710687abefa4929e60bf7
johnnyl25 <lemesle.johnny@live.fr>
2020-07-01 17:20:02 +0200

    modif image lorem

--- commands/random.js
+++ commands/random.js
@@ -5,15 +5,15 @@ exports.run = async (client, message, args) => {
     message.delete({ timeout: 1500 });
     if (args[0] === "lorem") {
         const lorem = await fetch("https://source.unsplash.com/1600x900/?")
-            // .then(res => res.json())
-            .then(json => json.download_url);
-            console.log(json.download_url);
-            // .then(function (json) {
-            //     const index = Math.floor(Math.random() * 100);
-            //     let url = new URL(json[index].download_url);
-            //     url = url.protocol + "//i." + url.host + url.pathname + ".jpg";
-            //     return url;
-            // });
+            .then(res => res.json())
+            // .then(json => json.download_url);
+            .then(function (json) {
+                const index = Math.floor(Math.random() * 100);
+                let url = new URL(json[index].download_url);
+                console.log(url);
+                url = url.protocol + "//i." + url.host + url.pathname + ".jpg";
+                return url;
+            });
         const embed = new RichEmbed()
             .setTitle("Image random")
             .setImage(lorem)
