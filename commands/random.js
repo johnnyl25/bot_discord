7ecbc6153377553eb923d2cd72f43cc6762f2231
johnnyl25 <lemesle.johnny@live.fr>
2020-07-01 17:32:54 +0200

    modif image lorem

--- commands/random.js
+++ commands/random.js
@@ -4,14 +4,13 @@ const { RichEmbed } = require("discord.js");
 exports.run = async (client, message, args) => {
     message.delete({ timeout: 1500 });
     if (args[0] === "lorem") {
-        const lorem = await fetch("https://source.unsplash.com/1600x900/?")
+        const lorem = await fetch("https://source.unsplash.com/1600x900/")
             .then(res => res.json())
             // .then(json => json.download_url);
             .then(function (json) {
                 const index = Math.floor(Math.random() * 100);
-                let url = new URL(json[index].download_url);
+                let url = new URL(json.download_url + index);
                 console.log(url);
-                url = url.protocol + "//i." + url.host + url.pathname + ".jpg";
                 return url;
             });
         const embed = new RichEmbed()
