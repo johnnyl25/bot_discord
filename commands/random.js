44a4d6ad2753cb2bb73d6cc1b487041a57e77a8a
johnnyl25 <lemesle.johnny@live.fr>
2020-08-01 18:25:18 +0200

    ajout command random nature

--- commands/random.js
+++ commands/random.js
@@ -12,13 +12,22 @@ exports.run = async (client, message, args) => {
                 let url = new URL(response.url);
                 console.log(url);
                 return url;
-            });
+            );
         const embed = new RichEmbed()
             .setTitle("Image random")
             .setImage(lorem)
             .setFooter("Powered by 'https://source.unsplash.com/'")
         message.channel.send(embed);
-    }
+    }  else if (args[0] === "nature") {
+        const index = Math.floor(Math.random() * 10);
+        const cat = await fetch("https://pixabay.com/api/?key=14869047-009b99445e3fcbb7651ac196a&category=nature")
+        .then(res => res.json())
+        .then(json => json.hits[index]['webformatURL']);
+        const embed = new RichEmbed()
+            .setTitle("Image random de la nature")
+            .setImage(cat)
+            .setFooter("Powered by 'https://pixabay.com/'")
+        message.channel.send(embed);
 }
 exports.help = {
     name: "random"
