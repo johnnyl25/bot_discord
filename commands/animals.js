6b432fb9444782365e7f05a7c6bae22343b19776
johnnyl25 <lemesle.johnny@live.fr>
2020-08-01 16:48:14 +0200

    debug api chat24

--- commands/animals.js
+++ commands/animals.js
@@ -6,13 +6,17 @@ exports.run = async (client, message, args) => {
 
     if (args[0] === "cat") {
         const cat = await fetch("https://pixabay.com/api/?key=14869047-009b99445e3fcbb7651ac196a&q=cat")
-            .then((response) => {
-                let url = new URL(response);
-                    response.hits.webformatURL();
-                    console.log(response);
+        .then(res => res.json())
+        .then(json => json.hits);
+        console.log(hits);
+        
+        // .then((response) => {
+            //     let url = new URL(response);
+            //         response.hits.webformatURL();
+            //         console.log(response);
                    
-                    return url;
-                });
+            //         return url;
+            //     });
             // .then(json => json);
         const embed = new RichEmbed()
             .setTitle("Image random de chat")
