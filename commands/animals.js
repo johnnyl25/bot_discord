4e791cc94445fa336de167382534a38a4e26c0ad
johnnyl25 <lemesle.johnny@live.fr>
2020-08-01 15:10:20 +0200

    debug api chat5

--- commands/animals.js
+++ commands/animals.js
@@ -7,7 +7,7 @@ exports.run = async (client, message, args) => {
     if (args[0] === "cat") {
         const cat = await fetch("https://pixabay.com/api/?key=14869047-009b99445e3fcbb7651ac196a&q=chat")
             .then(res => res.json())
-            .then(json => json.userImageURL);
+            // .then(json => json);
             console.log(json);
         const embed = new RichEmbed()
             .setTitle("Image random de chat")
